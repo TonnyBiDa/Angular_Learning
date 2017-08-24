9b602a6e7f4dead719e0a3d790d0871532558c02
毕达 <Tony@bidas-MacBook-Pro.local>
2017-08-24 18:58:49 +1200

    learnt component and try use ngModel two-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h3 (click)="onSelect()" [class.clicked]="showdetails===true">
+  			{{table.lastName}} {{table.firstName}}
+  			</h3>
+  			<input type="text" [(ngModel)]= "table.lastName" >
+  			<div *ngIf="showdetails===true">
+  				E-mail: {{table.email}} <br>
+  				phone: {{table.phone}}
+
+  			</div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  public table = {firstName:'Tony', lastName:'Bi', 
+  email:'tonnybida@gmail.com', phone:'021620075'};
+  public showdetails = false;
+  public i=0;
+  onSelect(){
+  	this.i++;
+  	console.log(this.i)
+  	if (this.i%2===0){
+  		console.log('good')
+  	this.showdetails = false;}else{this.showdetails = true}
+  }
+};
